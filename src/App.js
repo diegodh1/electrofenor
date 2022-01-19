f54905a8ab83ef6e2681dff356edf0d3c5059794
Diego Alexander Díaz Hernández <diegoalexanderdiazhernandez@Diegos-MacBook-Air.local>
2022-01-18 21:35:18 -0500

    new feature show offers

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import whatsapp from "./images/whatsapp.png";
 import "./App.css";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
-import Stack from "@mui/material/Stack";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@mui/material/Grid";
-import { GridEvents, useGridApiRef, DataGrid } from "@mui/x-data-grid";
-import Box from "@mui/material/Box";
-import Fab from "@mui/material/Fab";
+import { DataGrid } from "@mui/x-data-grid";
 import InputAdornment from "@mui/material/InputAdornment";
 import {
   AccountCircle,
-  AccessAlarm,
-  ThreeDRotation,
 } from "@mui/icons-material";
 import KeyIcon from "@mui/icons-material/Key";
 import Dialog from "@mui/material/Dialog";
@@ -24,7 +19,11 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import { style } from "@mui/system";
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,6 +38,40 @@ const useStyles = makeStyles((theme) => ({
     }
   },
 }));
+
+//tabs
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+      <div
+          role="tabpanel"
+          hidden={value !== index}
+          id={`simple-tabpanel-${index}`}
+          aria-labelledby={`simple-tab-${index}`}
+          {...other}
+      >
+        {value === index && (
+            <Box sx={{ p: 3 }}>
+              <Typography component={'span'} variant={'body2'}>{children}</Typography>
+            </Box>
+        )}
+      </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -70,7 +103,6 @@ const Alert = React.forwardRef(function Alert(props, ref) {
 });
 
 function App() {
-  const [clientId, setClientId] = useState("");
   const [open, setOpen] = useState(false);
   const [openSuccess, setOpenSuccess] = useState(false);
   const [openRegister, setOpenRegister] = useState(false);
@@ -79,6 +111,7 @@ function App() {
   const [isvalid, setIsValid] = useState(false);
   const [message, setMessage] = useState("");
   const [bills, setBills] = useState([]);
+  const [offers, setOffers] = useState([]);
   const [monto, setMonto] = useState(0);
   const [monto2, setMonto2] = useState(0);
   const [totalCartera, setTotalCartera] = useState(0);
@@ -97,10 +130,16 @@ function App() {
   const [masterPasswordRegistro, setMasterPasswordRegistro] = useState("");
   const [email, setEmail] = useState("");
   const [numeroFactura, setNumeroFactura] = useState("");
+  //tabs
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   const classes = useStyles();
 
-  const { height, width } = useWindowDimensions();
+  const { width } = useWindowDimensions();
   const sizeCols = width / 9.5;
 
   const columns = [
@@ -159,6 +198,46 @@ function App() {
     },
   ];
 
+  const columnsOffer = [
+    {
+      field: "cardCode",
+      headerName: "Cod. Usuario",
+      width: sizeCols,
+      editable: false,
+    },
+    {
+      field: "cardName",
+      headerName: "Nombre",
+      width: sizeCols,
+      editable: false,
+    },
+    {
+      field: "docDate",
+      headerName: "Fecha Documento",
+      width: sizeCols,
+      editable: false,
+    },
+    {
+      field: "docDueDate",
+      headerName: "Fecha Vencimiento Doc",
+      description: "This column has a value getter and is not sortable.",
+      sortable: true,
+      width: sizeCols,
+    },
+    {
+      field: "billNum",
+      headerName: "Num. Factura",
+      width: sizeCols,
+      editable: false,
+    },
+    {
+      field: "saldoAux",
+      headerName: "Saldo",
+      width: sizeCols,
+      editable: false,
+    },
+  ];
+
   useEffect(() => {
     const script = document.createElement("script");
 
@@ -168,7 +247,7 @@ function App() {
     document.body.appendChild(script);
     let userTmp = localStorage.getItem('username')
     let passTmp = localStorage.getItem('password')
-    if(userTmp!= undefined && userTmp != ''){
+    if(userTmp!== undefined && userTmp !== ''){
       const requestOptions = {
         method: "POST",
         headers: {
@@ -179,11 +258,12 @@ function App() {
       fetch("https://electrofrenorr.herokuapp.com/client/login", requestOptions)
         .then((response) => response.json())
         .then((data) => {
-          if (data.Status != 200) {
+          if (data.Status !== 200) {
             console.log(data.Message)
           } else {
             setLogged(true);
             getAllBillsByClient(userTmp, data.Payload.Token);
+            getAllOfferssByClient(userTmp, data.Payload.Token);
           }
         })
         .catch((error) => {
@@ -194,7 +274,6 @@ function App() {
   }, []);
 
   const getAllBillsByClient = (value, token2) => {
-    setClientId(value);
     const requestOptions = {
       method: "GET",
     };
@@ -236,7 +315,7 @@ function App() {
           setCupoAsignado(it["Cupo Asignado"]);
           tempDisponible = it["Cupo Asignado"];
           tempCartera = tempCartera + it.Saldo;
-          if (estado == "Vencida") {
+          if (estado === "Vencida") {
             tempVencido = tempVencido + it.Saldo;
           }
           return {
@@ -263,15 +342,54 @@ function App() {
       });
   };
 
+  const getAllOfferssByClient = (value, token2) => {
+    const requestOptions = {
+      method: "GET",
+    };
+    let url =
+        "https://electrofrenorr.herokuapp.com/cliente/ofertas/" +
+        value.toString();
+
+    console.log(url);
+    fetch(url, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          let temp = data.map(function (it) {
+            return {
+              cardCode: it.CardCode,
+              cardName: it.CardName,
+              docDate: it.DocDate,
+              docDueDate: it.DocDueDate,
+              billNum: it.DocNum,
+              saldo: it.Saldo,
+              saldoAux: "$" + new Intl.NumberFormat("en-IN").format(it.Saldo),
+              doc_entry: it.DocEntry
+            };
+          });
+          setOffers(temp);
+          console.log(temp);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  };
+
   const setDataWompi = (ids) => {
     setMonto(0);
     setMonto2(0);
     let data = [];
     let suma = 0;
     let ref = "";
-    let filtered = bills.filter(function (element) {
+    let filteredBills = bills.filter(function (element) {
       return ids.includes(element.billNum);
     });
+    let filteredOffers = offers.filter(function (element) {
+      return ids.includes(element.billNum);
+    });
+
+    let filtered = filteredBills.concat(filteredOffers)
+
     filtered.forEach((element) => {
       suma = suma + element.saldo;
       ref = element.cardCode;
@@ -291,7 +409,7 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (dataDetalle.length == 1) {
+    if (dataDetalle.length === 1) {
       let tmp = dataDetalle;
       tmp[0].amount = monto / 100;
     }
@@ -317,7 +435,7 @@ function App() {
     fetch("https://electrofrenorr.herokuapp.com/event/create", requestOptions)
       .then((response) => response.json())
       .then((data) => {
-        if (data.Status == 200) {
+        if (data.Status === 200) {
           e.target.submit();
         } else {
           setMessage(data.message);
@@ -357,7 +475,7 @@ function App() {
     fetch("https://electrofrenorr.herokuapp.com/client/login", requestOptions)
       .then((response) => response.json())
       .then((data) => {
-        if (data.Status != 200) {
+        if (data.Status !== 200) {
           setMessage(data.Message);
           setOpen(true);
         } else {
@@ -390,7 +508,7 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        if (data.Status != 201) {
+        if (data.Status !== 201) {
           setMessage(data.Message);
           setOpenRegister(false);
           setOpen(true);
@@ -424,7 +542,7 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        if (data.Status != 200) {
+        if (data.Status !== 200) {
           setMessage(data.Message);
           setOpenRegister(false);
           setOpen(true);
@@ -449,44 +567,93 @@ function App() {
     <div>
       {logged ? (
         <div className="App">
-          <div
-            style={{
-              height: 400,
-              width: "90%",
-              marginLeft: "5%",
-              marginTop: "2%",
-            }}
-          >
-            {bills.length > 0 ? (
-              <DataGrid
-                getRowId={(r) => r.billNum}
-                rows={bills}
-                onSelectionModelChange={(ids) => {
-                  console.log(ids);
-                  if (ids.length) {
-                    setDataWompi(ids);
-                  }
-                  else{
-                    setMonto(0);
-                    setMonto2(0);
-                    setIsValid(false)
-                  }
-                }}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection={true}
-                onRowEditCommit={(event) => console.log(event)}
-              />
-            ) : (
-              <h1 style={{ textAlign: "center" }}>
-                Estimado Cliente, No tiene facturas pendientes por pagar.
-              </h1>
-            )}
-          </div>
-          <br />
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{width: '90%', marginLeft:'2%', borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                <Tab label="FACTURAS" {...a11yProps(0)} />
+                <Tab label="OFERTAS" {...a11yProps(1)} />
+              </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+              <div
+                  style={{
+                    height: 400,
+                    width: "90%",
+                    marginLeft: "5%",
+                    marginTop: "2%",
+                  }}
+              >
+                {bills.length > 0 ? (
+                    <DataGrid
+                        getRowId={(r) => r.billNum}
+                        rows={bills}
+                        onSelectionModelChange={(ids) => {
+                          console.log(ids);
+                          if (ids.length) {
+                            setDataWompi(ids);
+                          }
+                          else{
+                            setMonto(0);
+                            setMonto2(0);
+                            setIsValid(false)
+                          }
+                        }}
+                        columns={columns}
+                        pageSize={5}
+                        rowsPerPageOptions={[5]}
+                        checkboxSelection={true}
+                        onRowEditCommit={(event) => console.log(event)}
+                    />
+                ) : (
+                    <p style={{ textAlign: "center" }}>
+                      Estimado Cliente, No tiene facturas pendientes por pagar.
+                    </p>
+                )}
+              </div>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <div
+                  style={{
+                    height: 400,
+                    width: "80%",
+                    marginLeft: "10%",
+                    marginTop: "2%",
+                  }}
+              >
+              {offers.length > 0 ? (
+                  <DataGrid
+                      getRowId={(r) => r.billNum}
+                      rows={offers}
+                      onSelectionModelChange={(ids) => {
+                        console.log(ids);
+                        if (ids.length) {
+                          setDataWompi(ids);
+                        }
+                        else{
+                          setMonto(0);
+                          setMonto2(0);
+                          setIsValid(false)
+                        }
+                      }}
+                      columns={columnsOffer}
+                      pageSize={5}
+                      rowsPerPageOptions={[5]}
+                      checkboxSelection={true}
+                      onRowEditCommit={(event) => console.log(event)}
+                  />
+              ) : (
+                  <p style={{ textAlign: "center" }}>
+                    Estimado Cliente, No tiene ofertas disponibles en el momento.
+                  </p>
+              )}
+              </div>
+            </TabPanel>
+          </Box>
+          <br/>
+          <br/>
+
           <Grid container spacing={2}>
-            <Grid item xs={1}></Grid>
+            <Grid item xs={1}/>
             <Grid item xs={1}>
               <img
                 src={whatsapp}
@@ -516,7 +683,7 @@ function App() {
                 </a>
               </p>
             </Grid>
-            <Grid item xs={1}></Grid>
+            <Grid item xs={1}/>
             <Grid item xs={4} style={{ textAlign: "left" }}>
               <p>Total Cartera {numberFormat2.format(totalCartera)} COP</p>
               <p style={{ color: "red" }}>
